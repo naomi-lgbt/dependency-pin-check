7c68d5638746fea704ed2034bac48230a5f488d9
Naomi Carrigan <nhcarrigan@gmail.com>
2023-03-18 19:46:43 -0700

    feat: validate package.json exists, and dependency fields

--- index.js
+++ index.js
@@ -1,28 +1,42 @@
+const { stat } = require('fs/promises');
 const { join } = require('path');
 (async () => {
     let failed = false;
     const path = join(process.cwd(), 'package.json');
+    const pathStat = await stat(path);
+    if (!pathStat.isFile()) {
+        console.log(`::error::No package.json found in ${process.cwd()}`);
+        process.exit(1);
+    }
     const packageJson = require(path);
     const { devDependencies, dependencies } = packageJson;
 
-    console.log(`Validating the following development dependencies:`)
+    if (devDependencies) {
+        console.log(`Validating the following development dependencies:`)
 
-    for (const [key, value] of Object.entries(devDependencies)) {
-        console.log(`Checking ${key} - ${value}...`)
-        if (/^\^/.test(value) || /^~/.test(value)) {
-            console.log(`::warning::devDependencies ${key} version is not fixed: ${value}`);
-            failed = true;
+        for (const [key, value] of Object.entries(devDependencies)) {
+            console.log(`Checking ${key} - ${value}...`)
+            if (/^\^/.test(value) || /^~/.test(value)) {
+                console.log(`::warning::devDependencies ${key} version is not fixed: ${value}`);
+                failed = true;
+            }
         }
+    } else {
+        console.log(`::debug::No devependencies found in package.json`)
     }
 
-    console.log(`Validating the following dependencies:`)
+    if (dependencies) {
+        console.log(`Validating the following dependencies:`)
 
-    for (const [key, value] of Object.entries(dependencies)) {
-        console.log(`Checking ${key} - ${value}...`)
-        if (/^\^/.test(value) || /^~/.test(value)) {
-            console.log(`::warning::dependencies ${key} version is not fixed: ${value}`);
-            failed = true;
+        for (const [key, value] of Object.entries(dependencies)) {
+            console.log(`Checking ${key} - ${value}...`)
+            if (/^\^/.test(value) || /^~/.test(value)) {
+                console.log(`::warning::dependencies ${key} version is not fixed: ${value}`);
+                failed = true;
+            }
         }
+    } else {
+        console.log(`::debug::No dependencies found in package.json`)
     }
 
     if (failed) {
