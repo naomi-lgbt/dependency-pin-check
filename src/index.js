bf023ae675fda82ea06c2c5a961d69d285d3ea58
Naomi Carrigan <commits@nhcarrigan.com>
2025-12-11 13:05:15 -0800

    feat: move javascript to dedicated file and export it

--- src/index.js
+++ src/index.js
@@ -1,51 +1,8 @@
 import core from "@actions/core";
-import { readFileSync } from "node:fs";
-import { join } from "node:path";
+import { javascriptDependencies } from "./langs/javascript.js";
 
-const checkForUnpinned = (dependencies) => {
-    return dependencies.filter(([_name, version]) => version.startsWith("^") || version.startsWith("~") || version.startsWith(">") || version.startsWith("="));
-}
+const language = core.getInput("language", { required: true });
 
-const checkDevDependencies = core.getBooleanInput("dev-dependencies", { required: true });
-const checkPeerDependencies = core.getBooleanInput("peer-dependencies", { required: true });
-const checkOptionalDependencies = core.getBooleanInput("optional-dependencies", { required: true });
-
-const packageFile = readFileSync(join(process.cwd(), "package.json"), "utf8");
-const packageJson = JSON.parse(packageFile);
-
-const dependencies = packageJson.dependencies;
-const devDependencies = packageJson.devDependencies;
-const peerDependencies = packageJson.peerDependencies;
-const optionalDependencies = packageJson.optionalDependencies;
-
-const unpinnedDependencies = dependencies ? checkForUnpinned(Object.entries(dependencies)) : [];
-const unpinnedDevDependencies = devDependencies ? checkForUnpinned(Object.entries(devDependencies)) : [];
-const unpinnedPeerDependencies = peerDependencies ? checkForUnpinned(Object.entries(peerDependencies)) : [];
-const unpinnedOptionalDependencies = optionalDependencies ? checkForUnpinned(Object.entries(optionalDependencies)) : [];
-
-const failures = {
-    dependencies: unpinnedDependencies,
-    devDependencies: unpinnedDevDependencies,
-    peerDependencies: unpinnedPeerDependencies,
-    optionalDependencies: unpinnedOptionalDependencies,
-}
-
-if (!checkDevDependencies) {
-    delete failures.devDependencies;
-}
-
-if (!checkPeerDependencies) {
-    delete failures.peerDependencies;
-}
-
-if (!checkOptionalDependencies) {
-    delete failures.optionalDependencies;
-}
-
-if (Object.values(failures).every(array => array.length === 0)) {
-    core.setOutput("success", true);
-    process.exit(0);
-} else {
-    core.setFailed(JSON.stringify(failures, null, 2));
-    process.exit(1);
-}
+if (language === "javascript") {
+    javascriptDependencies();
+};
